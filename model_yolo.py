fdec5da266d64b240c19b70aa43c1c2df28c88f2
Maks <46063054+mr1necs@users.noreply.github.com>
2024-12-05 12:06:44 +0300

    Update model_yolo.py

--- model_yolo.py
+++ model_yolo.py
@@ -4,23 +4,31 @@ from torch import cuda
 
 
 class YOLOModel:
+    """
+    Класс для управления моделью YOLO.
+    """
+
     def __init__(self, model_path):
         """
         Инициализация модели YOLO.
+        :param model_path: Путь к весам модели.
         """
+
         self.model = YOLO(model_path)
         self.device = 'cpu'
 
     def select_device(self, device_preference='cpu'):
         """
-        Устанавливает устройство для работы с моделью.
+        Устанавливает устройство для работы с моделью и перемещает модель на это устройство.
         :param device_preference: 'cpu', 'cuda', или 'mps'
         """
+
         self.device = (
             'mps' if device_preference == 'mps' and mps.is_available() else
             'cuda' if device_preference == 'cuda' and cuda.is_available() else
             'cpu'
         )
+        self.model.to(self.device)
 
     def detect(self, frame):
         """
@@ -28,4 +36,5 @@ class YOLOModel:
         :param frame: Входное изображение или сегмент.
         :return: Результаты детекции.
         """
+
         return self.model(frame)
